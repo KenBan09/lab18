7a04a9cfff8a0852fdb950ab6ee54453c2fa617f
KenBan09 <keeno.pkt@gmail.com>
2025-02-07 15:18:19 +0700

    1

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,7 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap( Rect R1 , Rect R2){
+	double xr = min(R1.x+R1.w , R2.x+R2.w) ;
+	double xl = max(R1.x,R2.x);
+	double yt = min(R1.y,R2.y) ;
+	double yb =  max(R1.y-R1.h , R2.y-R2.h);
+
+	if(xl<xr && yt>yb) return (xr - xl)* (yt - yb);
+	else return 0;
 
 
 
